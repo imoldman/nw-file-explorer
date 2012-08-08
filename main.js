f69ee7a2dc6b305f2b7da43f8fd5684fc1be4e99
Zhao Cheng <zcbenz@gmail.com>
2012-08-08 22:20:28 +0800

    AddressBar can navigate now

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ var folder_view = require('folder_view');
 var abar = require('address_bar');
 
 $(document).ready(function() {
+  global.$ = $;
+
   var folder = new folder_view.Folder($('#files'));
   var addressbar = new abar.AddressBar($('#addressbar'));
 
   folder.open(process.cwd());
   addressbar.set(process.cwd());
+
+  addressbar.on('navigate', function(dir) {
+    folder.open(dir);
+  });
 });
